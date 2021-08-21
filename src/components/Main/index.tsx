a97bf8d0eced7cef19eb2160a2c662d1b991e8fc
Chriszao <chris.f.assis18@gmail.com>
2021-08-20 23:18:53 -0300

    feat: Created and styled Main component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,24 @@
+import {
+  Wrapper,
+  Title,
+  Description,
+  Logo,
+  IllustrationStyled
+} from './styles';
+
 const Main = () => (
-  <main>
-    <h1>Christofer Assis</h1>
-  </main>
+  <Wrapper>
+    <Logo
+      src="/img/icon-192.png"
+      alt="Imagem de um átomo que representa a logo da biblioteca ReactJs."
+    />
+    <Title>Boiler plate para projetos Next.Js</Title>
+    <Description>Typescript, ReactJS, NextJS e Styled Components</Description>
+    <IllustrationStyled
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor em frente a tela com códigos"
+    />
+  </Wrapper>
 );
 
 export default Main;
