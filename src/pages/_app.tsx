bdbde4181be92269aaf7f6f766b663ded5d5dd94
Chriszao <chris.f.assis18@gmail.com>
2021-09-09 22:06:58 -0300

    feat: Configured PWA on boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,10 +7,9 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Boiler plate web projects</title>
-        <link rel="shortcut icon" href="/img/icon-512" />
-        <link rel="apple-touch-icon" href="/img/icon-512" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with Typescript, React, Next and Styled Components"
