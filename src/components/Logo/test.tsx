436bde9066eae53dc8d2b8569b43d3aa0e0869d7
Chriszao <chris.f.assis18@gmail.com>
2022-06-28 22:55:27 -0300

    feat: Add test case to hideOnMobile

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,5 +1,6 @@
 import { screen } from '@testing-library/react';
 import { renderWithTheme } from 'utils/tests/helpers';
+import 'jest-styled-components';
 
 import { Logo } from '.';
 
@@ -19,16 +20,27 @@ describe('<Logo />', () => {
   });
 
   it('should render a normal logo when size is default', () => {
-    renderWithTheme(<Logo color="black" />);
+    renderWithTheme(<Logo />);
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '11rem'
     });
   });
 
   it('should render a bigger logo', () => {
-    renderWithTheme(<Logo color="black" size="large" />);
+    renderWithTheme(<Logo size="large" />);
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       width: '20rem'
     });
+  });
+
+  it('should render a bigger logo without text if hideOnMobile', () => {
+    renderWithTheme(<Logo hideOnMobile />);
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyleRule(
+      'width',
+      '5.8rem',
+      {
+        media: '(max-width: 768px)'
+      }
+    );
   });
 });
